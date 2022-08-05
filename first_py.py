d7b855b8915affdee6baa0697f0251ac4499e76d
Nandor Toth <totnanoff@gmail.com>
2022-08-05 16:38:13 +0200

    Fibo talan. Fixes #12

--- first_py.py
+++ first_py.py
@@ -22,17 +22,16 @@ def minus(a,b):
     return a-b
 
 def div(a,b):
-<<<<<<< HEAD
+
     return a/b
-=======
-    return a/b
+
 
 def multi(a,b):
     return a*b + 1
 #bello
 #Hello
-<<<<<<< HEAD
->>>>>>> 101d08c8d7d5715315c0fef76447783764c476fa
-=======
+
 a = [1,2,3,4]
->>>>>>> 38b770a6cb2d73c527a8c893cc440821ddb1f4f9
+
+def fib(n):
+    return n
